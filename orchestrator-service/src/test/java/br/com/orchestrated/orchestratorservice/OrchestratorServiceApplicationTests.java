3bbd5956c4c411d26b2611fc76e80bd321cd9f61
hscastro <halisonsc5@gmail.com>
2023-12-11 20:43:54 -0300

    first commit

---
+++ orchestrator-service/src/test/java/br/com/orchestrated/orchestratorservice/OrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orchestrated.orchestratorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/br/com/orchestrated/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orchestrated.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

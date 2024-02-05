fbe0625ba123543d3c4c317e16df9fb5e5b2e364
hscastro <halisonsc5@gmail.com>
2024-02-05 12:05:15 -0300

    Ajustes no EnumTopics

--- orchestrator-service/src/main/java/br/com/orchestrated/orchestratorservice/EnumTopics.java
+++ orchestrator-service/src/main/java/br/com/orchestrated/orchestratorservice/EnumTopics.java
@@ -1,18 +1,16 @@
 package br.com.orchestrated.orchestratorservice;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public enum EnumTopics {
 
     START_SAGA("start-saga"),
     BASE_ORCHESTRATOR("orchestrator"),
-    FINISH_SUCCESS("finis_success"),
+    FINISH_SUCCESS("finish-success"),
     FINISH_FAIL("finish-fail"),
-    PRODUCT_VALIDATION_SUCCESS("produt-validation-success"),
-    PRODUCT_VALIDATION_FAIL("produt-validation-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("product-validation-fail"),
 
     PAYMENT_SUCCESS("payment-success"),
     PAYMENT_FAIL("payment-fail"),
